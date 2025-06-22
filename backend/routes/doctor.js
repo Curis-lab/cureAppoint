674043e4aac2adb253e1ac524d35c5c8d3c71c38
Boris <72118807+Curis-lab@users.noreply.github.com>
2025-06-22 07:27:02 +0630

    setup backend

--- backend/routes/doctor.js
+++ backend/routes/doctor.js
@@ -5,12 +5,16 @@ import {
   getSingleDoctor,
 } from "../controllers/doctorController.js";
 import { Router } from "express";
+import { authenticate, restrict } from "../auth/verifyToken.js";
+import reviewRouter from './review.js';
 
 const router = Router();
+//nested router
+router.use('/:doctorId/reviews', reviewRouter);
 
 router.get("/:id", getSingleDoctor);
 router.get("/", getAllDoctor);
-router.put("/:id", updateDoctor);
-router.delete("/:id", deleteDoctor);
+router.put("/:id", authenticate, restrict(["doctor"]), updateDoctor);
+router.delete("/:id", authenticate, restrict(["doctor"]), deleteDoctor);
 
 export default router;
